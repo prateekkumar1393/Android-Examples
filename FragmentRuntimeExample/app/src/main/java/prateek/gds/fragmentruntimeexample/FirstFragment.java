75727228186bb24ef4cd887e07b382b9be9f0a2a
Prateek Kumar <prateekkumar1393@gmail.com>
2020-05-28 15:53:40 +0530

    swtiching three fragments on a one activity at runtime

---
+++ FragmentRuntimeExample/app/src/main/java/prateek/gds/fragmentruntimeexample/FirstFragment.java
@@ -0,0 +1,33 @@
+package prateek.gds.fragmentruntimeexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment {
+
+    private Button button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        button = view.findViewById(R.id.b2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new SecondFragment(), null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        return view;
+    }
+}
